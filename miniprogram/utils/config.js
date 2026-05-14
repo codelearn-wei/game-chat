3dcf58d226c03bf063185df4d6c00a4b670e54cc
weijinyuan <11741889+weijinyuan@user.noreply.gitee.com>
2026-05-14 15:34:24 +0800

    config: 切换后端到 Render (https://game-chat-28fa.onrender.com)

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -3,7 +3,7 @@
 // 模式B（体验版，当前）: Render 稳定后端，地址不会变，关机后测试者仍可用
 // 切换方式: 将 BASE_URL 改为对应地址后，微信开发者工具重新「编译」
 
-const BASE_URL = 'RENDER_URL_PLACEHOLDER';
+const BASE_URL = 'https://game-chat-28fa.onrender.com';
 
 module.exports = {
   BASE_URL,
